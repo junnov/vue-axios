4bd70e967961fdb841380a540c089a40c05d96db
junnov <firebolt21v@gmail.com>
2022-08-29 01:37:00 +0900

    update: make axios get and post

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,25 @@
 <template>
+<div>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <TestPage></TestPage>
+</div>
+  
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import TestPage from "./components/TestPage.vue"
+
+
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+    TestPage,
+  },
+  
 }
 </script>
