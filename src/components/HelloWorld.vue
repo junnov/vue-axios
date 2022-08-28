daeafbee7681159416e80834a8f3f816ef490cc2
junnov <firebolt21v@gmail.com>
2022-08-29 01:37:00 +0900

    update: make axios get and post

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,10 @@
 <template>
+<div>
   <h1>{{ msg }}</h1>
   <button @click="count++">count is: {{ count }}</button>
   <p>Edit <code>components/HelloWorld.vue</code> to test hot module replacement.</p>
+</div>
+  
 </template>
 
 <script>
---
+++ src/components/TestPage.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <v-btn @click="testGet">Get Me</v-btn>
+    <v-btn @click="testPost">Post Me!</v-btn>
+  </div>
+</template>
+<script>
+import axios from "axios"
+
+export default {
+  methods: {
+    testGet (){
+      // Make a request for a user with a given ID
+      axios.get('https://reqres.in/api/users?page=2')
+        .then(res => {
+          // handle success
+          console.log(res);
+        })
+        .catch(err => {
+          // handle error
+          console.log(err);
+        })
+        .then(() => {
+          // always executed
+          console.log("Get Action!")
+        });
+    },
+    testPost(){
+      axios.post("https://reqres.in/api/register",{
+          "email": "eve.holt@reqres.in",
+          "password": "pistol"
+      })
+      .then(res => {
+        console.log(res)
+      })
+      .catch(err=> {
+        console.log(err)
+      })
+      .then(()=>{
+        console.log("Post Action!")
+      })
+    }
+  }
+}
+</script>
